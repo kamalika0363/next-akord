81f7b8a80cdfdf74652eca25ea72ecab2fb7f176
kamalika0363 <kamalikaghora14@gmail.com>
2024-03-14 00:13:05 +0530

    Vault Display: Active Inactive

---
+++ app/(vaults)/(routes)/vaults/active/page.tsx
@@ -0,0 +1,59 @@
+"use client";
+import {useState, useEffect} from "react";
+import {Akord, Auth} from '@akord/akord-js';
+
+const Active = () => {
+    const [loggedIn, setLoggedIn] = useState<boolean>(true);
+    const [vaults, setVaults] = useState<Array<any>>([]);
+    const [activeSide, setActiveSide] = useState<'ACTIVE'>('ACTIVE');
+
+    useEffect(() => {
+        const fetchVaults = async () => {
+            try {
+                if (loggedIn) {
+                    const {wallet} = await Auth.authenticate();
+                    const akord = await Akord.init(wallet);
+                    const allVaults = await akord.vault.listAll();
+                    const activeVaults = allVaults.filter((vault) => vault.status === 'ACTIVE');
+                    setVaults(activeVaults);
+                } else {
+                    console.log("User not logged in");
+                }
+            } catch (error) {
+                console.error("Error fetching vaults:", error);
+            }
+        };
+
+        fetchVaults();
+    }, [loggedIn]);
+
+    return (
+        <div className="w-screen">
+            <div
+                className={`transition-all duration-500 ${activeSide === 'ACTIVE'}`}>
+                <table style={{width: '100%', justifyContent: 'start'}}>
+                    <thead>
+                    <tr>
+                        <th style={{textAlign: 'start', padding: '8px'}}>Name</th>
+                        <th style={{textAlign: 'start', padding: '8px'}}>Description</th>
+                        <th style={{textAlign: 'start', padding: '8px'}}>Created At</th>
+                        <th style={{textAlign: 'start', padding: '8px'}}>Updated At</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {vaults.map((vault) => (
+                        <tr className="border border-gray-800" key={vault.id}>
+                            <td style={{textAlign: 'start', padding: '8px'}}>{vault.name}</td>
+                            <td style={{textAlign: 'start', padding: '8px'}}>{vault.description}</td>
+                            <td style={{textAlign: 'start', padding: '8px'}}>{vault.createdAt}</td>
+                            <td style={{textAlign: 'start', padding: '8px'}}>{vault.updatedAt}</td>
+                        </tr>
+                    ))}
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    );
+}
+
+export default Active;
--- app/(vaults)/(routes)/vaults/page.tsx
+++ app/(vaults)/(routes)/vaults/page.tsx
@@ -1,80 +1,73 @@
 "use client";
-import {useState} from "react";
-import {Akord, Auth} from '@akord/akord-js';
+import React, { useState, useEffect } from "react";
+import { Akord, Auth } from '@akord/akord-js';
+import Active from "@/app/(vaults)/(routes)/vaults/active/page";
+import InActive from "@/app/(vaults)/(routes)/vaults/inactive/page";
 
 const Vault = () => {
     const [email, setEmail] = useState<string>('');
     const [pass, setPass] = useState('');
     const [loggedIn, setLoggedIn] = useState<boolean>(true);
     const [vaults, setVaults] = useState<Array<any>>([]);
+    const [activeSide, setActiveSide] = useState<'ACTIVE' | 'ARCHIVED'>('ACTIVE');
 
-    const showVaults = async () => {
-        try {
-            if (loggedIn) {
-                const {wallet} = await Auth.authenticate();
-                const akord = await Akord.init(wallet);
-                const vaults = await akord.vault.listAll();
-                const vaultDetails = vaults.map((vault) => ({
-                    id: vault.id,
-                    name: vault.name,
-                    status: vault.status,
-                    description: vault.description,
-                    createdAt: vault.createdAt,
-                    updatedAt: vault.updatedAt,
-                }));
-                setVaults(vaultDetails);
-            } else {
-                console.log("User not logged in");
+    useEffect(() => {
+        const fetchVaults = async () => {
+            try {
+                if (loggedIn) {
+                    const { wallet } = await Auth.authenticate();
+                    const akord = await Akord.init(wallet);
+                    const vaults = await akord.vault.listAll();
+                    const vaultDetails = vaults.map((vault) => ({
+                        id: vault.id,
+                        name: vault.name,
+                        status: vault.status,
+                        type: vault.tags,
+                        description: vault.description,
+                        createdAt: vault.createdAt,
+                        modifiedOn: vault.updatedAt,
+                    }));
+                    setVaults(vaultDetails);
+                } else {
+                    console.log("User not logged in");
+                }
+            } catch (error) {
+                console.error("Error fetching vaults:", error);
             }
-        } catch (error) {
-            console.error("Error fetching vaults:", error);
-        }
+        };
+
+        fetchVaults();
+    }, [loggedIn]);
+
+    const handleSideToggle = () => {
+        setActiveSide(activeSide === 'ACTIVE' ? 'ARCHIVED' : 'ACTIVE');
     };
 
     return (
-        <div className="flex flex-col gap-4">
-            <h1 className="font-bold text-2xl">Vault</h1>
-            {loggedIn && (
-                <div>
-                    <button className="mb-4" onClick={showVaults}>Show Vault</button>
-                    <div className="flex flex-row gap-8">
-                        <div>
-                            {vaults.filter((vault) => vault.status === 'ACTIVE').length > 0 && (
-                                <div>
-                                    <h2>Active Vaults</h2>
-                                    <ul>
-                                        {vaults
-                                            .filter((vault) => vault.status === 'ACTIVE')
-                                            .map((vault) => (
-                                                <li key={vault.id}>
-                                                    <ul>{vault.name}</ul>
-                                                    <ul>{vault.description}</ul>
-                                                </li>
-                                            ))}
-                                    </ul>
-                                </div>
-                            )}
+        <div className="w-full">
+            <div className="flex flex-col gap-4 h-full">
+                <h1 className="font-bold text-2xl">Vault</h1>
+                {loggedIn && (
+                    <div className="flex flex-col gap-8 h-full">
+                        <div className="flex gap-4 border-b transition-all duration-500 text-base">
+                            <button onClick={handleSideToggle}
+                                    className={`focus:outline-none ${activeSide === 'ACTIVE' ? 'text-gray-600 border-b-2 border-orange-900' : 'text-gray-400 border-transparent hover:border-orange-800'}`}>
+                                Active
+                            </button>
+                            <button onClick={handleSideToggle}
+                                    className={`focus:outline-none ${activeSide === 'ARCHIVED' ? 'text-gray-600 border-b-2 border-orange-900' : 'text-gray-400 border-transparent hover:border-orange-800'}`}>
+                                Inactive
+                            </button>
                         </div>
-                        <div>
-                            {vaults.filter((vault) => vault.status === 'ARCHIVED').length > 0 && (
-                                <div>
-                                    <h2>Inactive Vaults</h2>
-                                    <ul>
-                                        {vaults
-                                            .filter((vault) => vault.status === 'ARCHIVED')
-                                            .map((vault) => (
-                                                <li key={vault.id}>
-                                                    <ul>{vault.name}</ul>
-                                                    <ul>{vault.description}</ul>
-                                                </li>
-                                            ))}
-                                    </ul>
-                                </div>
-                            )}
+                        <div className="gap-8 h-full">
+                            <div
+                                className={`transition-all duration-500 h-full ${activeSide === 'ACTIVE' ? 'ml-0' : '-ml-full'}`}>
+                                {activeSide === 'ACTIVE' ? <Active/> : <InActive/>}
+                            </div>
                         </div>
                     </div>
-                </div>
-            )}
+                )}
+            </div>
         </div>
     );
 };
